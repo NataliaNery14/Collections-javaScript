5fa6685625e2c62c93f35505b5e81882a6650f56
Natália Nery <112709527+NataliaNery14@users.noreply.github.com>
2022-09-22 15:28:46 -0300

    Add files via upload

---
+++ exercicio Collection 22 de setembro.ts
@@ -0,0 +1,13 @@
+import promtSync = requare ('Prompt-Sync');
+const promp = promtSync ();
+
+const estoque = ["moquila, calsa, sapato"];
+estoque.push["meia"];
+console.log('${estoque}');
+
+estoque.splice(1,2);
+console.log(`${estoque}`);
+
+
+
+
